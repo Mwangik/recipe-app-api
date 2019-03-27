110d3df7cd4f28a32b1ac7dd2cd2677dc37687bb
Mwangik <gkmwangi0@gmail.com>
2019-03-27 13:54:28 +0300

    added feature to list tags

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -1,4 +1,4 @@
-from  django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import TestCase
 
@@ -10,7 +10,7 @@ from core.models import Tag
 from recipe.serializers import TagSerializer
 
 
-TAG_URL = reverse('recipe:tag-list')
+TAGS_URL = reverse('recipe:tag-list')
 
 
 class PublicTagsApiTests(TestCase):
